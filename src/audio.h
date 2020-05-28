a1a303b7be68d9ebc6919b71ef3a63ab33a17381
zh2eou <zh2eou@gmail.com>
2020-05-27 21:00:21 -0400

    placeholder class for audio player object

---
+++ src/audio.h
@@ -0,0 +1,20 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#include <vector>
+#include "SDL.h"
+#include "snake.h"
+
+class AudioPlayer {
+ public:
+  AudioPlayer();
+  ~AudioPlayer();
+ 
+  // play sound effect
+  // play music (maybe try with csound)
+  
+ private:
+
+};
+
+#endif
